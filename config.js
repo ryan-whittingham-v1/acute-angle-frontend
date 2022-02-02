39f469f814212fdf20d8a7db134e135c3d24cc4e
Ryan Whittingham <whittingham777@gmail.com>
2022-02-01 22:28:27 -0800

    update backend url

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:3000/api/graphql`;
-export const prodEndpoint = `https://acute-angle.herokuapp.com/api/graphql`;
+export const prodEndpoint = `https://acute-angle-backend-5b9lk.ondigitalocean.app/`;
 export const perPage = 4;
